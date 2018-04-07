4650bc01522dbdcb6b857afc12f01ebcd180d31a
Denise Pendleton <dpendl1@hotmail.com>
2018-04-07 19:36:44 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,13 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
@@ -13,6 +15,14 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      @@cart.each do |c|
+        resp.write "#{c}\n"
+      end
+    elsif req.path.match(/add/)
+      potential_item = req.params["item"]
+
+      if exists?
     else
       resp.write "Path Not Found"
     end
@@ -20,6 +30,14 @@ class Application
     resp.finish
   end
 
+  def exists?(item)
+    if @@items.include?(item)
+      true
+    else
+      false
+    end
+  end
+  
   def handle_search(search_term)
     if @@items.include?(search_term)
       return "#{search_term} is one of our items"
